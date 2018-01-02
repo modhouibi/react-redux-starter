31a13cdd85d0ed46f30150c5dfd940286927a162
modhouibi <m.dhouibi.dev@gmail.com>
2018-01-02 01:10:20 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+
+
+class App extends Component{
+	
+	constructor(){
+		super();
+		
+	}
+	
+	render(){
+		
+		return(
+			<div>
+			APP
+			</div>
+		);
+	}
+}
+
+export default App;
